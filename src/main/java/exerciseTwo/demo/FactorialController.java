f181449b4ebd588b20878383677b20563ac99e56
Edduard79 <eduard.22@gmail.com>
2023-04-12 20:31:54 +0200

    First Try

---
+++ src/main/java/exerciseTwo/demo/FactorialController.java
@@ -0,0 +1,19 @@
+package exerciseTwo.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FactorialController {
+
+    @GetMapping("/factorial/{n}")
+    public int getFactorial(@PathVariable int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
